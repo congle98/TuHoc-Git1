4fd3164e52e1044c5c102ef22503346055bc1f07
congle98 <79907265+congle98@users.noreply.github.com>
2021-06-23 21:18:07 +0700

    cong1 commit5

--- src/com/app/model/Cong1.java
+++ src/com/app/model/Cong1.java
@@ -1,6 +1,10 @@
 package com.app.model;
 
 public class Cong1 {
-
+    private String name;
+    private int age;
+    private String address;
+    private String address3;
+    private String address1111;
 
 }
